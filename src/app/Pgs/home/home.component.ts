0b3eea3b6c43ada4861b68638b663583010ecbc6
Steven <scarvajalusb@gmail.com>
2020-09-20 15:19:03 -0500

    visualizacion de los dispositivos con sus dueños en la pagina admin

--- src/app/Pgs/home/home.component.ts
+++ src/app/Pgs/home/home.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/Services/auth.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  constructor(private authService: AuthService, private router: Router) {}
 
   ngOnInit(): void {
+    this.authService.hasUser().subscribe((data) => {
+      if (data) {
+        console.log('logueado, entra');
+      } else {
+        console.log('deslogueado, logueate porfavor');
+        this.router.navigate(['/login']);
+      }
+    });
   }
-
 }
